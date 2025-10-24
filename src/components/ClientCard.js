a773f70afc5f7c99913a3abe2589939167469a7a
Bruno Piorno <brunopiornop@gmail.com>
2025-10-24 09:28:25 -0300

    estilo card

--- src/components/ClientCard.js
+++ src/components/ClientCard.js
@@ -7,7 +7,6 @@ const ClientCard = memo(({ client, index }) => {
 
   const componentProps = {
     className: "client-card",
-    initial: { opacity: 0, y: 20 },
     whileInView: { opacity: 1, y: 0 },
     viewport: { once: true },
     transition: { duration: 0.6, delay: index * 0.1 },
