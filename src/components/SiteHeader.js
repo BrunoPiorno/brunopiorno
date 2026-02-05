e6e5c34c792e05978f2418a4704ca9366de417d1
Bruno Piorno Polucci <brunopiornop@gmail.com>
2026-02-05 18:24:04 -0300

    Update SiteHeader.js

--- src/components/SiteHeader.js
+++ src/components/SiteHeader.js
@@ -11,6 +11,7 @@ const SiteHeader = ({ hideMenu = false }) => {
   const location = useLocation();
   const [isScrolled, setIsScrolled] = useState(false);
   const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+  
 
   useEffect(() => {
     const handleScroll = () => {
