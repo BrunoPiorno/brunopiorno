da5bbc98a39f8e4f3d8fd4ddf455ad0d62286702
Bruno Piorno Polucci <brunopiornop@gmail.com>
2026-02-25 20:06:53 -0300

    Update AppRoutes.js

--- src/AppRoutes.js
+++ src/AppRoutes.js
@@ -11,12 +11,6 @@ const LegalNotice = lazy(() => import('./components/LegalNotice'));
 const CookiesPolicy = lazy(() => import('./components/CookiesPolicy'));
 const ThankYouPage = lazy(() => import('./components/ThankYouPage'));
 const WhatsAppGenerator = lazy(() => import('./components/WhatsAppGenerator'));
-const Soluciones = lazy(() => import('./components/Soluciones'));
-const DesarrolloWeb = lazy(() => import('./components/DesarrolloWeb'));
-const LandingPages = lazy(() => import('./components/LandingPages'));
-const AplicacionesWeb = lazy(() => import('./components/AplicacionesWeb'));
-const Ecommerce = lazy(() => import('./components/Ecommerce'));
-const GoogleAds = lazy(() => import('./components/GoogleAds'));
 const ProposalLanding = lazy(() => import('./components/proposals/ProposalLanding'));
 const PresentationPage = lazy(() => import('./components/PresentationPage'));
 
@@ -24,12 +18,6 @@ const AppRoutes = () => (
   <Suspense fallback={<Loader />}>
     <Routes>
       <Route path="/" element={<HomePage />} />
-      <Route path="/soluciones" element={<Soluciones />} />
-      <Route path="/soluciones/desarrollo-web" element={<DesarrolloWeb />} />
-      <Route path="/soluciones/landing-pages" element={<LandingPages />} />
-      <Route path="/soluciones/aplicaciones-web" element={<AplicacionesWeb />} />
-      <Route path="/soluciones/ecommerce" element={<Ecommerce />} />
-      <Route path="/soluciones/google-ads" element={<GoogleAds />} />
       <Route path="/whatsapp-generator" element={<WhatsAppGenerator />} />
       <Route path="/blog" element={<BlogList />} />
       <Route path="/blog/:slug" element={<BlogPost />} />
